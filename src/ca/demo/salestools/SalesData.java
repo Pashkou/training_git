41f971d9daced46ee9e275ec8de2c226944b059b
Siarhei Pashkou <pashkou.s@gmail.com>
2017-03-12 15:26:27 +0100

    summed up values when display

--- src/ca/demo/salestools/SalesData.java
+++ src/ca/demo/salestools/SalesData.java
@@ -1,12 +1,17 @@
 package ca.demo.salestools;
-
+/**
+ * represents sales data for a given period.
+ * */
 public class SalesData {
 	int data[] = {0, 4, 42};
 	
 	public void display(){
 		System.out.println("Data: ");
+		int sum = 0;
 		for(int i = 0; i < data.length; i++){
 			System.out.println("Next value " + data[i]);
+			sum += i;
 		}
+		System.out.println("Sum is " + sum);
 	}
 }
