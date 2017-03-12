0dfbe8bbde2aae571a0ca6b147274135c698ac4e
Siarhei Pashkou <pashkou.s@gmail.com>
2017-03-12 15:11:28 +0100

    add sales data, update main

--- src/ca/demo/salestools/Main.java
+++ src/ca/demo/salestools/Main.java
@@ -3,8 +3,16 @@ package ca.demo.salestools;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
 
+	private static void displayGreeting() {
+		System.out.println("Hello happy sales people!");
+		System.out.println("This app shows sales data.");
+		System.out.println("--------------------------");
 	}
 
 }
